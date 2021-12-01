6458371d25a4541f0897c811e6654f301d22b7e1
Jeremiah Peschka <jpeschka@stackoverflow.com>
2021-12-01 15:33:44 -0500

    Day 1

--- AdventOfCode2021/Day1.cs
+++ AdventOfCode2021/Day1.cs
@@ -1,6 +1,22 @@
-﻿namespace AdventOfCode2021;
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace AdventOfCode2021;
 
 public class Day1
 {
-    
+    public int CountDescending(string lines)
+    {
+        return CountDescending(lines.Split("\n").Select(x => Int32.Parse(x)).ToArray());
+    }
+    public int CountDescending(int[] entries)
+    {
+        return entries.Select((x, i) => i < entries.Length - 1 && entries[i + 1] > entries[i]).Count(x => x);
+    }
+
+    public int CountChunks(string lines)
+    {
+        var entries = lines.Split("\n").Select(x => Int32.Parse(x)).ToArray();
+        return CountDescending( entries.Select((x, i) => i < entries.Length - 2 ? entries[i..(i + 3)].Sum() : 0).ToArray());
+    }
 }
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2021;
+
 Console.WriteLine("Hello, World!");
+
+var d = new Day1();
+// Console.WriteLine(d.CountDescending(Inputs.Day1));
+
+Console.WriteLine(d.CountChunks(Inputs.Day1));
