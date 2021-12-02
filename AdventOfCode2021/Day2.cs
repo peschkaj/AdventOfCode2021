7555b48ffcaeaaad7c91436e078e4b5de8431c1d
Jeremiah Peschka <jpeschka@stackoverflow.com>
2021-12-02 11:32:45 -0500

    Day 2

---
+++ AdventOfCode2021/Day2.cs
@@ -0,0 +1,79 @@
+﻿namespace AdventOfCode2021;
+
+public class Day2
+{
+    public int Part1(string input)
+    {
+        var lines = input.Split("\n")
+            .Select(x =>
+            {
+                var words = x.Split(" ");
+                return (words[0], int.Parse(words[1]));
+            });
+        var destination = Moves(lines);
+        return destination.horizontal * destination.vertical;
+    }
+    
+    private (int horizontal, int vertical) Moves(IEnumerable<(string, int)> instructions)
+    {
+        int h = 0, v = 0;
+        foreach (var (direction, distance) in instructions)
+        {
+            switch (direction)
+            {
+                case "forward": 
+                    h += distance;
+                    break;
+                case "up":
+                    v -= distance;
+                    break;
+                case "down":
+                    v += distance;
+                    break;
+            }
+        }
+        
+        return (h,v);
+    }
+
+    public int Part2(string input)
+    {
+        var lines = input.Split("\n")
+            .Select(x =>
+            {
+                var words = x.Split(" ");
+                return (words[0], int.Parse(words[1]));
+            });
+        var destination = ProcessMoves(lines);
+        return destination.position * destination.depth;
+    }
+
+    private (int position, int depth) ProcessMoves(IEnumerable<(string, int)> instructions)
+    {
+        var aim = 0;
+        var position = 0;
+        var depth = 0;
+        
+        foreach (var (direction, distance) in instructions)
+        {
+            switch (direction)
+            {
+                case "forward": 
+                    position += distance;
+                    if (aim > 0)
+                    {
+                        depth += aim * distance;
+                    }
+                    break;
+                case "up":
+                    aim -= distance;
+                    break;
+                case "down":
+                    aim += distance;
+                    break;
+            }
+        }
+        
+        return (position,depth);
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -2,9 +2,10 @@
 
 using AdventOfCode2021;
 
-Console.WriteLine("Hello, World!");
+//var d = new Day1();
+//Console.WriteLine(d.CountDescending(Inputs.Day1));
+//Console.WriteLine(d.CountChunks(Inputs.Day1));
 
-var d = new Day1();
-// Console.WriteLine(d.CountDescending(Inputs.Day1));
-
-Console.WriteLine(d.CountChunks(Inputs.Day1));
+var d = new Day2();
+Console.WriteLine(d.Part1(Inputs.Day2));
+Console.WriteLine(d.Part2(Inputs.Day2));
