5d52c44e57571d7065fc831e26c4e3ff2ffa9efb
Jeremiah Peschka <jpeschka@stackoverflow.com>
2021-12-03 16:26:01 -0500

    Day 3 Part 1

---
+++ AdventOfCode2021/Day3.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+
+namespace AdventOfCode2021;
+
+public class Day3
+{
+    public int Part1(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        var lineLength = lines.First().Length;
+        var bits = new int[lineLength];
+        foreach (var line in lines)
+        {
+            for (int i = 0; i < lineLength; i++)
+            {
+                if (line[i] == '1')
+                {
+                    bits[i]++;
+                }
+            }
+        }
+
+        var gamma = "";
+        var epsilon = "";
+
+        foreach (var t in bits)
+        {
+            if (t > (lines.Length / 2))
+            {
+                gamma += "1";
+                epsilon += "0";
+            }
+            else
+            {
+                gamma += "0";
+                epsilon += "1";
+            }
+        }
+
+        return Convert.ToInt32(gamma, 2) * Convert.ToInt32(epsilon, 2);
+    }
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -6,6 +6,27 @@ using AdventOfCode2021;
 //Console.WriteLine(d.CountDescending(Inputs.Day1));
 //Console.WriteLine(d.CountChunks(Inputs.Day1));
 
-var d = new Day2();
-Console.WriteLine(d.Part1(Inputs.Day2));
-Console.WriteLine(d.Part2(Inputs.Day2));
+// var d = new Day2();
+// Console.WriteLine(d.Part1(Inputs.Day2));
+// Console.WriteLine(d.Part2(Inputs.Day2));
+
+var d = new Day3();
+Console.WriteLine(d.Part1(Inputs.Day3));
+int? x = 1;
+var y = 0;
+var lol = "";
+
+if (lol is null)
+{
+    
+}
+
+try
+{
+    Console.WriteLine(x / y);
+}
+catch (Exception ex)
+{
+    ex.Data["lol"] = "omg";
+    throw;
+}
